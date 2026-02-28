7614178e4444253561628cfd89f889e9fd0658f5
Kenzie-Crawford <kenzie.scrawford13@gmail.com>
2026-02-28 12:40:27 -0500

    greater than or less then 10 activity

---
+++ PA4.1/src/LessThanOrGreaterThanTen.java
@@ -0,0 +1,13 @@
+public class LessThanOrGreaterThanTen {
+    static void main(String[] args) {
+        int x = 15;
+
+        if(x < 10){
+            System.out.println("Less than 10");
+
+        } else {
+            System.out.println("Greater than 10");
+        }
+
+    }
+}
