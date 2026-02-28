64ddb79149dd3d4f19f4b4d570e869adf1bce4cb
Kenzie-Crawford <kenzie.scrawford13@gmail.com>
2026-02-28 12:51:56 -0500

    Range activity

---
+++ PA4.1/src/Range.java
@@ -0,0 +1,11 @@
+public class Range {
+    static void main(String[] args) {
+        int x = 15;
+
+        if (x < 10 || x > 20) {
+            System.out.println("Out of range");
+        } else  {
+            System.out.println("In range");
+        }
+    }
+}
