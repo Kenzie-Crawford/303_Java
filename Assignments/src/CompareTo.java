741177bf351567f12582e22cefce1a1e2d6e3401
Kenzie-Crawford <kenzie.scrawford13@gmail.com>
2026-02-28 10:45:44 -0500

    guided labs compare contains replace and substrings

---
+++ Assignments/src/CompareTo.java
@@ -0,0 +1,9 @@
+public class CompareTo {
+    public static void main(String args[]) {
+        String s1 = "hello";
+        String s2 = "hello";
+        String s3 = "hemlo";
+        String s4 = "flag";
+        System.out.println(s1.compareTo(s2));
+    }
+}
