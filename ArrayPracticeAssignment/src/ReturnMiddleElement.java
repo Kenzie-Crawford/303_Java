3817dbb0ba886e7a24029d48fd4c9c5190edb452
Kenzie-Crawford <kenzie.scrawford13@gmail.com>
2026-03-02 15:57:33 -0500

    print the middle element assignment

---
+++ ArrayPracticeAssignment/src/ReturnMiddleElement.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class ReturnMiddleElement {
+    public static void main(String[] args) {
+
+        int[] arr = {13, 5, 7, 68, 2};
+        int middleIndex = arr.length / 2;
+
+        System.out.println(arr[middleIndex]);
+
+
+    }
+}
