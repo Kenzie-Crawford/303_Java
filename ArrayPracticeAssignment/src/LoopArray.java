43aba87570020816b67f42b792ba35f38248da2b
Kenzie-Crawford <kenzie.scrawford13@gmail.com>
2026-03-02 16:24:55 -0500

    loop array assignment

---
+++ ArrayPracticeAssignment/src/LoopArray.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class LoopArray {
+    public static void main(String[] args) {
+        int [] array = new int[5];
+        int index = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+}
