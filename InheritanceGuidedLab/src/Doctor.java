c0b64b830478ff4b34fb9fa2d19d09897ca40287
Kenzie-Crawford <kenzie.scrawford13@gmail.com>
2026-03-03 15:39:58 -0500

    Inheritance guided lab and SBA practice

---
+++ InheritanceGuidedLab/src/Doctor.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+
+public class Doctor {
+    String DoctorName;
+    String Department;
+    public void Doctor_Details() {
+        System.out.println("Doctor Details...");
+
+    }
+}
---
+++ SBAPractice/src/SumOfDigitsGreaterThan5.java
@@ -0,0 +1,4 @@
+package PACKAGE_NAME;
+
+public class SumOfDigitsGreaterThan5 {
+}
