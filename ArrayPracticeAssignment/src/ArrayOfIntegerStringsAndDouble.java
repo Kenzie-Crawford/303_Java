b00d3b2a7591900ce6c990aa0e96dafb4c88fab5
Kenzie-Crawford <kenzie.scrawford13@gmail.com>
2026-03-03 08:45:04 -0500

    weird array

---
+++ ArrayPracticeAssignment/src/ArrayOfIntegerStringsAndDouble.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class ArrayOfIntegerStringsAndDouble {
+    public static void main(String[] args) {
+        Object[] array = {10, "dog", "cat", "horse", 3.14};
+        System.out.println(Arrays.toString(array));
+
+    }
+}
